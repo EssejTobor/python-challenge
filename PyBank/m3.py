822bfd6c283c058c72ac357cfc4c23368617c5b2
Jesse Kowalcic <jesseckowalcic@gmail.com>
2019-12-03 20:47:30 -0500

    Update m3.py

--- PyBank/m3.py
+++ PyBank/m3.py
@@ -1,32 +1,32 @@
+#Dependencies
 import os
 import csv
 
+# Data to read in and analyze
 f = open(r'C:\My-Repo\python-challenge\python-challenge\PyBank\budget_data.csv')
 csv = csv.reader(f)
 csv_header = next(csv)
+
+# Stored Variables
 months = []
 pandf = []
 
 for row in csv:
     months.append(row[0])
     pandf.append(row[1])
-
+    
+#convert variable data type
 profits_losses = [int(i) for i in pandf] 
 
-def percentChange(startPoint,currentPoint):
-    return((float(currentPoint)-startPoint)/abs(startPoint))*100.00
-for eachN in profits_losses:
-    pc = percentChange(profits_losses[0], eachN)
-    print(pc)
 
 
-# $-2315.12
-#print(average_diff)
 
-'''
+
+
+
 print("""
 Financial Analysis
 ----------------------------------------
 """)
 print(f'Total Months: {len(months)}')
-print(f'Total: ${sum(profits_losses)}')'''
+print(f'Total: ${sum(profits_losses)}')
